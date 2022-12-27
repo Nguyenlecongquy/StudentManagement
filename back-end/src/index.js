61b0b5908dd21d35bf8c239678eef79d5fd7ea8f
20120563 <20120563@student.hcmus.edu.vn>
2022-12-27 22:02:24 +0700

    fix controller

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,56 +1,67 @@
 const express = require('express');
 const exhbs = require('express-handlebars');
-const session = require("express-session");
+const session = require('express-session');
 const morgan = require('morgan');
 const path = require('path');
 const route = require('./routes/index');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
-   origin: "http://127.0.0.1:5173",
-   credentials: true
-}))
+app.use(
+	cors({
+		origin: 'http://127.0.0.1:5173',
+		credentials: true,
+	})
+);
 
-
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //http logger
 app.use(morgan('dev'));
 
 //body parser
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 //xu li du lieu request post json
-app.use(express.json())
-
+app.use(express.json());
 
 // Dung session
 app.use(
-   session({
-       secret: "tritranmn2",
-       resave: false,
-       saveUninitialized: true,
-       cookie: { secure: false },
-   })
+	session({
+		secret: 'tritranmn2',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: false },
+	})
 );
 
-
 //engine
-app.engine('hbs', exhbs.engine({
-   extname: '.hbs'
-}));
+app.engine(
+	'hbs',
+	exhbs.engine({
+		extname: '.hbs',
+	})
+);
 app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname,'resources','views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
-app.use((err,req,res,next)=>{
-   const statusCode= err.statusCode | 500;
-   res.status(statusCode).send(err.message);
-})
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode | 500;
+	res.status(statusCode).send(err.message);
+});
+
+const abc = require('./models/score.m');
+const de = require('./models/student.m');
+const cs = require('./models/summary.m');
+var a = abc.findScores('LOP0001111', 'MH00001111', 'HK1').then((a) => {
+	console.log('a', a);
+});
+var b = cs.findSummary('LOP0001111', 'MH00001111', 'HK1', 5).then((b) => {
+	console.log('b', b);
+});
 
 const port = 3000;
-app.listen(port,()=>console.log(`Server listening at http://localhost:${port}`))
-
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
--- back-end/src/models/summary.m.js
+++ back-end/src/models/summary.m.js
@@ -1,17 +1,49 @@
-
-const DB = require('../connect/db')
+const DB = require('../connect/db');
 var db;
 try {
-    db = DB.connect;
-    console.log("summary model connect database successfully!")
+	db = DB.connect;
+	console.log('summary model connect database successfully!');
 } catch (error) {
-    console.log("summary model connect database fail!")
+	console.log('summary model connect database fail!');
 }
 
 const summaryModel = {
+	fieldClassId: (classId) => (classId == undefined ? true : "hs.ma_lop = '" + classId + "'"),
+	fieldSubjectId: (subjectId) =>
+		subjectId == undefined ? true : "d.ma_mh = '" + subjectId + "'",
 
-    
+	findSummary: async (classId, subjectId, semester, minMark) => {
+		const soLuongDatValue = await db.any(
+			`SELECT count(*) as number
+         FROM diem d JOIN hoc_sinh hs ON d.ma_hs = hs.ma_hs
+         WHERE ${summaryModel.fieldClassId(classId)}
+         AND ${summaryModel.fieldSubjectId(subjectId)}
+         AND d.hoc_ki = $1
+         AND d.diem_tk >= $2 ;`,
+			[semester, minMark]
+		);
 
+		const lopValue = await db.any(
+			`SELECT *
+            from lop
+            where ma_lop = $1`,
+			[classId]
+		);
+
+		var lop = lopValue[0].ten_lop; //lớp
+		var siSo = lopValue[0].si_so_lop; //sĩ số
+		var soLuongDat = soLuongDatValue[0].number;
+		var tiLe = (soLuongDat / siSo) * 100; //tỉ lệ đơn vị là %
+
+		var result = {
+			lop,
+			siSo,
+			soLuongDat,
+			tiLe,
+		};
+
+		return result;
+	},
 };
 
 module.exports = summaryModel;
